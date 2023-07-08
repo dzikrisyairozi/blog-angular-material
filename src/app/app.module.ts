550ffcf821351964d7f8aaddc1c7b622410ea8f4
dikiogres <dikiogres@gmail.com>
2023-07-09 00:29:20 +0700

    feat: login page route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialDesignModule} from '../app/material-design/material-design.module'
+import {MaterialDesignModule} from '../app/material-design/material-design.module';
+import { LoginComponent } from './auth/login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
