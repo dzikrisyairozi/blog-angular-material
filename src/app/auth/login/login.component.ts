5c8c037f3415ddf9f533cbfdd8b95c216f575ac3
dikiogres <dikiogres@gmail.com>
2023-07-09 08:45:30 +0700

    chore: tailwindcss

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['../../app.component.css']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
