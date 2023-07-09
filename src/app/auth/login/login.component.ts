980a3098ad1b93cf9bfda65b2562146524076a50
dikiogres <dikiogres@gmail.com>
2023-07-09 08:59:38 +0700

    feat: redirect to home when login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  isLoading: boolean = false
+
+  constructor( private router: Router){
+
+  }
+  
+  ngOnInit(): void{}
+
+  login(){
+    this.isLoading=true;
+    setTimeout(()=>{
+      this.router.navigate(['/']);
+    }, 2000)
+  }
 }
