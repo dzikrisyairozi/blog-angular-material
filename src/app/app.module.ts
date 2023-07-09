0abc63ddd312f922a168a00a32622e96cc7170c0
dikiogres <dikiogres@gmail.com>
2023-07-09 12:05:22 +0700

    feat: grid list blogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialDesignModule} from '../app/material-design/material-design.module';
+import { MaterialDesignModule } from '../app/material-design/material-design.module';
 import { LoginComponent } from './auth/login/login.component';
-import { BlogsComponent } from './blogs/blogs/blogs.component';
 import { BlogPageComponent } from './blogs/blog-page/blog-page.component';
-import { SingleBlogComponent } from './blogs/single-blog/single-blog.component'
+import { SingleBlogComponent } from './blogs/single-blog/single-blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BlogsComponent,
     BlogPageComponent,
-    SingleBlogComponent
+    SingleBlogComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialDesignModule
+    MaterialDesignModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
